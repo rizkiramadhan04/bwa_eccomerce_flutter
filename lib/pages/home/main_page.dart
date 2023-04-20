27cc6f26b5e94b7d9838755ac899096497b87562
Rizki Ramadhan <rizki@nadyne.net>
2023-04-20 16:34:42 +0700

    Update terbaru

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -1,14 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bwa_ecommerce/theme.dart';
+import 'package:provider/provider.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
   @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
   Widget build(BuildContext context) {
+    Widget cartButton() {
+      return FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: secondaryColor,
+        child: Image.asset(
+          'assets/order_icon.png',
+          width: 120,
+        ),
+      );
+    }
+
+    Widget customeBottomNav() {
+      return ClipRRect(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(30),
+        ),
+        child: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          notchMargin: 12,
+          clipBehavior: Clip.antiAlias,
+          child: BottomNavigationBar(
+            backgroundColor: backgroundColor4,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                icon: Container(
+                  margin: EdgeInsets.only(
+                    top: 20,
+                    bottom: 10,
+                  ),
+                  child: Image.asset(
+                    'assets/home_icon.png',
+                    width: 21,
+                    color: Color(0xff808191),
+                  ),
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  margin: EdgeInsets.only(
+                    top: 20,
+                    bottom: 10,
+                  ),
+                  child: Image.asset(
+                    'assets/chat_icon.png',
+                    width: 21,
+                    color: Color(0xff808191),
+                  ),
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  margin: EdgeInsets.only(
+                    top: 20,
+                    bottom: 10,
+                  ),
+                  child: Image.asset(
+                    'assets/favorite_icon.png',
+                    width: 21,
+                    color: Color(0xff808191),
+                  ),
+                ),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  margin: EdgeInsets.only(
+                    top: 20,
+                    bottom: 10,
+                  ),
+                  child: Image.asset(
+                    'assets/profile_icon.png',
+                    width: 21,
+                    color: Color(0xff808191),
+                  ),
+                ),
+                label: '',
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
-      body: Center(
-        child: Text('Main Page'),
-      ),
+      backgroundColor: backgroundColor3,
+      floatingActionButton: cartButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: customeBottomNav(),
     );
   }
 }
